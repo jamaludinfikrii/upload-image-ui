082fe82a04f6c6f968d59840321fe7f1579366aa
Fikri <jamesfikrii@gmail.com>
2019-04-04 11:58:26 +0700

    edit image done

--- src/App.js
+++ src/App.js
@@ -1,14 +1,43 @@
 import React, { Component } from 'react';
 import './App.css';
 import Axios from 'axios';
+import { Button, Modal, ModalHeader, ModalBody, ModalFooter } from 'reactstrap';
 
 class App extends Component {
-  state = {selectedFile : null}
+  state = {selectedFile : null, data : [] , modal : false , dataEdit : {}, selectedFileEdit : null}
+
+  componentDidMount(){
+    this.getData()
+  }
+  getData = () => {
+    Axios.get('http://localhost:4000/getAllData')
+    .then((res) => this.setState({data:res.data}))
+    .catch((err) => console.log(err))
+  }
   onChangeHandler = (event) => {
     // UNTUK MENDAPATKAN FILE IMAGE
      this.setState({selectedFile : event.target.files[0]}) 
 
   }
+
+  renderJsx = () => {
+    var jsx = this.state.data.map((val,index) => {
+      return(
+        <tr>
+          <td>{index + 1}</td>
+          <td> {val.product_name} </td>
+          <td> {val.product_price} </td>
+          <td> <img src = {'http://localhost:4000/' + val.product_image} 
+                width = '50px' />  
+          </td>
+          <td> <input type = 'button' className='btn btn-info' onClick={() => this.setState({modal:true, dataEdit :val})} value='edit' /> </td>
+          <td> <input type = 'button' className='btn btn-danger' value='delete' /> </td>        
+        </tr>
+      )
+    })
+    return jsx
+  }
+
   valueHandler =() => {
     var value = this.state.selectedFile ? this.state.selectedFile.name : 'Pick A Picture'
     return value
@@ -30,6 +59,44 @@ class App extends Component {
      })
   }
 
+  onChangeHandlerEdit = (event) => {
+    // UNTUK NGE GET VALUE FILES
+    this.setState({selectedFileEdit : event.target.files[0]})
+  }
+
+  valueHandlerEdit =() => {
+    var value = this.state.selectedFileEdit ? this.state.selectedFileEdit.name : 'Pick A Picture'
+    return value 
+  }
+
+  onSaveBtnClick = () => {
+    var newData = {
+      product_name : this.refs.namaEdit.value ? this.refs.namaEdit.value : this.state.dataEdit.product_name,
+      harga : this.refs.hargaEdit.value ? this.refs.hargaEdit.value : this.state.dataEdit.product_price
+    }
+    if(this.state.selectedFileEdit){
+      var fd = new FormData()
+      fd.append('edit' , this.state.selectedFileEdit)
+      fd.append('data' ,JSON.stringify(newData))
+      
+      // UNTUK DAPETIN PATH IMAGE YANG MAU DIHAPUS
+      fd.append('imageBefore' , this.state.dataEdit.product_image)
+      Axios.put('http://localhost:4000/addProduct/' + this.state.dataEdit.id , fd)
+      .then((res) => {
+        alert(res.data)
+        this.setState({modal : false})
+        this.getData()
+      })
+    }else{
+      Axios.put('http://localhost:4000/addProduct/' + this.state.dataEdit.id , newData)
+      .then((res) => {
+        alert(res.data)
+        this.setState({modal : false})
+        this.getData()
+      })
+    }
+  }
+
   render() {
     return (
       <div className="container">
@@ -49,6 +116,45 @@ class App extends Component {
           </div>
 
         </div>
+        <table className='table mt-5'>
+          <tr>
+            <td>NO</td>
+            <td>NAMA</td>
+            <td>HARGA</td>
+            <td>IMAGE</td>
+            <td>EDIT</td>
+            <td>DELETE</td>
+          </tr>
+          {this.renderJsx()}
+        </table>
+
+        {/* =================== MODAL EDIT */}
+        <div>
+          <Modal isOpen={this.state.modal} toggle={() => this.setState({modal:false})} className={this.props.className}>
+            <ModalHeader toggle={() => this.setState({modal:false})}>Edit Product ~ {this.state.dataEdit.product_name}</ModalHeader>
+            <ModalBody>
+              <div className='row'> 
+                <div className='col-md-3'>
+                  <img src={'http://localhost:4000/' + this.state.dataEdit.product_image} width='100%' alt='broken' />
+                  <input type='file' onChange={this.onChangeHandlerEdit} style={{display:'none'}} ref='inputEdit' />
+                  <input type='button' value={this.valueHandlerEdit()} className='btn btn-primary' 
+                         onClick={ () =>this.refs.inputEdit.click()} />
+                </div>
+                <div className='col-md-9'> 
+                  <input type='text' className='form-control' 
+                  placeholder={this.state.dataEdit.product_name} ref='namaEdit' />
+                  <input type='number' className='form-control mt-3' 
+                  placeholder={this.state.dataEdit.product_price} ref='hargaEdit' />
+                </div>
+              </div>
+            </ModalBody>
+            <ModalFooter>
+              <Button color="primary" onClick={this.onSaveBtnClick}>Save</Button>{' '}
+              <Button color="secondary" onClick={() => this.setState({modal:false})}>Cancel</Button>
+            </ModalFooter>
+          </Modal>
+        </div>
+
       </div>
     );
   }
